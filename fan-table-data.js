52fe7c22894a8c8469073234fa40b891f2e45068
tui9bao <yanyilui02@gmail.com>
2026-04-28 17:08:43 +0800

    Add files via upload
    publish first version

---
+++ fan-table-data.js
@@ -0,0 +1,992 @@
+window.FAN_TABLE = [
+  {
+    "category": "基本",
+    "name": "底",
+    "points": "5",
+    "description": "食出任何糊時自動加上的番數"
+  },
+  {
+    "category": "花",
+    "name": "無花",
+    "points": "1",
+    "description": "沒有任何花牌"
+  },
+  {
+    "category": "花",
+    "name": "正花",
+    "points": "2",
+    "description": "摸到符合自己方位的花牌"
+  },
+  {
+    "category": "花",
+    "name": "爛花",
+    "points": "1",
+    "description": "摸到非自己方位的花牌"
+  },
+  {
+    "category": "花",
+    "name": "一台花",
+    "points": "10",
+    "description": "集齊「春夏秋冬」或「梅蘭竹菊」4隻同系列花牌"
+  },
+  {
+    "category": "花",
+    "name": "一台草",
+    "points": "3",
+    "description": "集齊任意四隻花牌（不限系列）"
+  },
+  {
+    "category": "花",
+    "name": "七隻花",
+    "points": "20",
+    "description": "擁有七隻花，可不理手牌視為自摸。可決定採用「七隻花/七搶一/一搶七」中的任一規則"
+  },
+  {
+    "category": "花",
+    "name": "七搶一",
+    "points": "30",
+    "description": "自己拿著6隻花，另一家已有1隻花，當自己摸到第7隻時，可選擇不理手牌直接食糊，視為另一家出銃"
+  },
+  {
+    "category": "花",
+    "name": "一搶七",
+    "points": "30",
+    "description": "另一家已有7隻花，自己摸到最後1隻花時，可選擇不理手牌直接食糊，視為另一家出銃"
+  },
+  {
+    "category": "花",
+    "name": "花糊/八仙過海",
+    "points": "40",
+    "description": "集齊全部八隻花牌"
+  },
+  {
+    "category": "字牌",
+    "name": "正圈正位風",
+    "points": "3",
+    "description": "擁有一組同時符合該局圈風和自己方位的「東/南/西/北」"
+  },
+  {
+    "category": "字牌",
+    "name": "正風(圈/位)",
+    "points": "2",
+    "description": "擁有一組符合該局圈風或自己方位的「東/南/西/北」"
+  },
+  {
+    "category": "字牌",
+    "name": "爛風",
+    "points": "1",
+    "description": "擁有一組普通「東/南/西/北」，沒有符合該局圈風或自己方位"
+  },
+  {
+    "category": "字牌",
+    "name": "無字",
+    "points": "1",
+    "description": "全副牌沒有番子牌"
+  },
+  {
+    "category": "字牌",
+    "name": "無字花",
+    "points": "5",
+    "description": "全副牌沒有番子牌及花牌"
+  },
+  {
+    "category": "字牌",
+    "name": "三元牌(中發白)",
+    "points": "2",
+    "description": "擁有一組「中/發/白」"
+  },
+  {
+    "category": "字牌",
+    "name": "小三元",
+    "points": "20",
+    "description": "「中/發/白」其中兩組刻子/槓子＋一對眼"
+  },
+  {
+    "category": "字牌",
+    "name": "大三元",
+    "points": "60",
+    "description": "「中/發/白」三組刻子/槓子"
+  },
+  {
+    "category": "字牌",
+    "name": "小三風",
+    "points": "20",
+    "description": "「東/南/西/北」其中兩組刻子/槓子＋一對眼"
+  },
+  {
+    "category": "字牌",
+    "name": "大三風",
+    "points": "40",
+    "description": "「東/南/西/北」三組刻子/槓子"
+  },
+  {
+    "category": "字牌",
+    "name": "小四喜",
+    "points": "80",
+    "description": "東/南/西/北」三組刻子/槓子，一對眼"
+  },
+  {
+    "category": "字牌",
+    "name": "大四喜",
+    "points": "120",
+    "description": "東/南/西/北」四組刻子/槓子"
+  },
+  {
+    "category": "自摸",
+    "name": "自摸",
+    "points": "1",
+    "description": "自己摸到食糊的牌"
+  },
+  {
+    "category": "自摸",
+    "name": "花上自摸",
+    "points": "1",
+    "description": "補花自摸"
+  },
+  {
+    "category": "自摸",
+    "name": "花上自摸 X2",
+    "points": "10",
+    "description": "補花後補花自摸"
+  },
+  {
+    "category": "自摸",
+    "name": "花上自摸 X3",
+    "points": "30",
+    "description": "連續三次摸到花牌後補牌自摸"
+  },
+  {
+    "category": "自摸",
+    "name": "花上自摸 X4",
+    "points": "60",
+    "description": "連續四次摸到花牌後補牌自摸"
+  },
+  {
+    "category": "自摸",
+    "name": "花上自摸 Xn",
+    "points": "80",
+    "description": "連續多次（n次）摸到花牌後補牌自摸"
+  },
+  {
+    "category": "自摸",
+    "name": "花上槓上自摸",
+    "points": "10",
+    "description": "補花後開槓，補槓自摸"
+  },
+  {
+    "category": "自摸",
+    "name": "槓上自摸",
+    "points": "1",
+    "description": "補槓自摸"
+  },
+  {
+    "category": "自摸",
+    "name": "槓上自摸 X2",
+    "points": "30",
+    "description": "連續兩次開槓補牌後自摸"
+  },
+  {
+    "category": "自摸",
+    "name": "槓上自摸 X3",
+    "points": "40",
+    "description": "連續三次開槓補牌後自摸"
+  },
+  {
+    "category": "自摸",
+    "name": "槓上自摸 X4",
+    "points": "80",
+    "description": "連續四次開槓補牌後自摸"
+  },
+  {
+    "category": "自摸",
+    "name": "槓上自摸 X5",
+    "points": "120",
+    "description": "連續五次開槓補牌後自摸"
+  },
+  {
+    "category": "自摸",
+    "name": "槓上花上自摸",
+    "points": "10",
+    "description": "補槓後摸到花牌，補花自摸"
+  },
+  {
+    "category": "眼",
+    "name": "將眼",
+    "points": "1",
+    "description": "使用 2、5、8 作為眼（對子）"
+  },
+  {
+    "category": "眼",
+    "name": "對碰",
+    "points": "1",
+    "description": "以兩對眼叫糊"
+  },
+  {
+    "category": "眼",
+    "name": "假獨",
+    "points": "1",
+    "description": "可以堆砌出以單吊/偏章/卡窿食糊，實際叫糊叫多於一飛"
+  },
+  {
+    "category": "眼",
+    "name": "獨獨",
+    "points": "2",
+    "description": "叫糊只叫一飛，以單吊/偏章/卡窿食糊"
+  },
+  {
+    "category": "狀態",
+    "name": "門清",
+    "points": "3",
+    "description": "沒有碰、槓或吃，所有牌均由自己摸入，其他玩家出銃"
+  },
+  {
+    "category": "狀態",
+    "name": "不求人",
+    "points": "5",
+    "description": "門清且自摸食糊"
+  },
+  {
+    "category": "狀態",
+    "name": "半求人",
+    "points": "8",
+    "description": "整副牌倚靠別家打出的牌上、碰（不可槓），最後一隻單吊自摸"
+  },
+  {
+    "category": "狀態",
+    "name": "全求人",
+    "points": "15",
+    "description": "整副牌倚靠別家打出的牌上、碰（不可槓），最後一隻單吊由其他玩家出銃"
+  },
+  {
+    "category": "聽牌",
+    "name": "叮牌/聽",
+    "points": "5",
+    "description": "輪到自己時宣布叫糊，凍結手牌不可再轉章，因此可以旁觀其他玩家的手牌"
+  },
+  {
+    "category": "聽牌",
+    "name": "叮即/聽牌即食",
+    "points": "10",
+    "description": "叮牌後在一巡內食糊，包括自摸；又名「一發」"
+  },
+  {
+    "category": "聽牌",
+    "name": "門叮",
+    "points": "15",
+    "description": "門清狀態下叮牌"
+  },
+  {
+    "category": "聽牌",
+    "name": "天聽/天叮",
+    "points": "40",
+    "description": "開局時在莊家打出第1隻牌前，宣告叮牌"
+  },
+  {
+    "category": "聽牌",
+    "name": "地聽/地叮",
+    "points": "30",
+    "description": "莊家打出第1張牌時，宣告叮牌"
+  },
+  {
+    "category": "聽牌",
+    "name": "人聽/人叮",
+    "points": "30",
+    "description": "第一巡內，閒家打出第1張牌時，宣告叮牌；若有人上、碰、槓牌即破"
+  },
+  {
+    "category": "速食",
+    "name": "四隻內",
+    "points": "40",
+    "description": "牌海只有4隻牌以內食糊，允許中途有人上、碰、槓牌"
+  },
+  {
+    "category": "速食",
+    "name": "七隻內",
+    "points": "20",
+    "description": "牌海只有7隻牌以內食糊，允許中途有人上、碰、槓牌"
+  },
+  {
+    "category": "速食",
+    "name": "十隻內",
+    "points": "10",
+    "description": "牌海只有10隻牌以內食糊，允許中途有人上、碰、槓牌"
+  },
+  {
+    "category": "速食",
+    "name": "天糊",
+    "points": "120",
+    "description": "莊家起手即食糊"
+  },
+  {
+    "category": "速食",
+    "name": "地糊",
+    "points": "100",
+    "description": "莊家打出的第一隻牌，閒家即食糊"
+  },
+  {
+    "category": "速食",
+    "name": "人糊",
+    "points": "80",
+    "description": "閒家摸第一隻牌即自摸，但如果中途有人上、碰、槓牌即破，只計四隻內"
+  },
+  {
+    "category": "尾牌",
+    "name": "河底撈魚",
+    "points": "5",
+    "description": "最後四隻牌自摸"
+  },
+  {
+    "category": "尾牌",
+    "name": "海底撈魚",
+    "points": "10",
+    "description": "最後一隻牌自摸"
+  },
+  {
+    "category": "尾牌",
+    "name": "海底撈月",
+    "points": "20",
+    "description": "最後一隻牌自摸一筒"
+  },
+  {
+    "category": "平糊/對對糊",
+    "name": "平糊",
+    "points": "3",
+    "description": "除了一對眼外，其餘組合全是順子（上牌），沒有碰/槓"
+  },
+  {
+    "category": "平糊/對對糊",
+    "name": "無字花平糊",
+    "points": "15",
+    "description": "平糊並且沒有番子牌和花牌；又名「大平糊」/「無字花大平」"
+  },
+  {
+    "category": "刻",
+    "name": "兩暗刻",
+    "points": "3",
+    "description": "兩組門前清的刻子，包括暗槓"
+  },
+  {
+    "category": "刻",
+    "name": "三暗刻",
+    "points": "15",
+    "description": "三組門前清的刻子，包括暗槓"
+  },
+  {
+    "category": "刻",
+    "name": "四暗刻",
+    "points": "30",
+    "description": "四組門前清的刻子，包括暗槓"
+  },
+  {
+    "category": "刻",
+    "name": "五暗刻",
+    "points": "80",
+    "description": "五組門前清的刻子，包括暗槓"
+  },
+  {
+    "category": "刻",
+    "name": "坎坎糊",
+    "points": "120",
+    "description": "門清自摸對對糊，沒有槓牌；等於自摸五暗刻"
+  },
+  {
+    "category": "槓",
+    "name": "明槓",
+    "points": "1",
+    "description": "手持3隻相同的牌，別家打出第4隻時，即宣告「槓」"
+  },
+  {
+    "category": "槓",
+    "name": "暗槓",
+    "points": "2",
+    "description": "自己摸到4隻相同的牌，宣告「暗槓」；在該局完結時須亮牌讓其他玩家知道"
+  },
+  {
+    "category": "槓",
+    "name": "三槓子",
+    "points": "30",
+    "description": "三組槓子，明暗皆可"
+  },
+  {
+    "category": "槓",
+    "name": "四槓子",
+    "points": "100",
+    "description": "四組槓子，明暗皆可"
+  },
+  {
+    "category": "槓",
+    "name": "廿二羅漢",
+    "points": "120",
+    "description": "五組槓子，明暗皆可"
+  },
+  {
+    "category": "兄弟/姊妹",
+    "name": "兩兄弟",
+    "points": "3",
+    "description": "兩組相同數字、不同門的刻子/槓子"
+  },
+  {
+    "category": "兄弟/姊妹",
+    "name": "小三兄弟",
+    "points": "10",
+    "description": "三組相同數字、不同門的刻子/槓子/眼，其中一對為眼"
+  },
+  {
+    "category": "兄弟/姊妹",
+    "name": "大三兄弟",
+    "points": "15",
+    "description": "三組相同數字、不同門的刻子/槓子"
+  },
+  {
+    "category": "兄弟/姊妹",
+    "name": "兩姊妹",
+    "points": "3",
+    "description": "兩組同門、數字相連的刻子/槓子"
+  },
+  {
+    "category": "兄弟/姊妹",
+    "name": "小三姊妹",
+    "points": "8",
+    "description": "三組同門、數字相連的刻子/槓子/眼，其中一對為眼"
+  },
+  {
+    "category": "兄弟/姊妹",
+    "name": "大三姊妹",
+    "points": "15",
+    "description": "三組同門、數字相連的刻子/槓子"
+  },
+  {
+    "category": "兄弟/姊妹",
+    "name": "小四姊妹",
+    "points": "20",
+    "description": "四組同門、數字相連的刻子/槓子/眼，其中一對為眼"
+  },
+  {
+    "category": "兄弟/姊妹",
+    "name": "大四姊妹",
+    "points": "40",
+    "description": "四組同門、數字相連的刻子/槓子"
+  },
+  {
+    "category": "兄弟/姊妹",
+    "name": "小五姊妹",
+    "points": "60",
+    "description": "五組同門、數字相連的刻子/槓子/眼，其中一對為眼"
+  },
+  {
+    "category": "兄弟/姊妹",
+    "name": "大五姊妹",
+    "points": "80",
+    "description": "五組同門、數字相連的刻子/槓子"
+  },
+  {
+    "category": "般高",
+    "name": "明一般高",
+    "points": "5",
+    "description": "兩組同門、一樣數字的順子，其中有明牌（123123筒）"
+  },
+  {
+    "category": "般高",
+    "name": "暗一般高",
+    "points": "10",
+    "description": "兩組同門、一樣數字的順子，全部藏於手牌（123123筒）"
+  },
+  {
+    "category": "般高",
+    "name": "明雙般高",
+    "points": "15",
+    "description": "明一般高 ×2門（123123筒+234234索）"
+  },
+  {
+    "category": "般高",
+    "name": "暗雙般高",
+    "points": "30",
+    "description": "暗一般高 ×2門（123123筒+234234索）"
+  },
+  {
+    "category": "般高",
+    "name": "明三般高",
+    "points": "15",
+    "description": "三組同門、一樣數字的順子，其中有明牌（123123123筒）"
+  },
+  {
+    "category": "般高",
+    "name": "暗三般高",
+    "points": "30",
+    "description": "三組同門、一樣數字的順子，全部藏於手牌（123123123筒）"
+  },
+  {
+    "category": "般高",
+    "name": "明四般高",
+    "points": "30",
+    "description": "四組同門、一樣數字的順子，其中有明牌（123123123123筒）"
+  },
+  {
+    "category": "般高",
+    "name": "暗四般高",
+    "points": "60",
+    "description": "四組同門、一樣數字的順子，全部藏於手牌（123123123123筒）"
+  },
+  {
+    "category": "相逢/同順",
+    "name": "明二相逢",
+    "points": "3",
+    "description": "兩組不同門、一樣數字的順子，其中有明牌"
+  },
+  {
+    "category": "相逢/同順",
+    "name": "暗二相逢",
+    "points": "5",
+    "description": "兩組不同門、一樣數字的順子，全部藏於手牌"
+  },
+  {
+    "category": "相逢/同順",
+    "name": "明三相逢",
+    "points": "10",
+    "description": "三組不同門、一樣數字的順子，其中有明牌"
+  },
+  {
+    "category": "相逢/同順",
+    "name": "暗三相逢",
+    "points": "15",
+    "description": "三組不同門、一樣數字的順子，全部藏於手牌"
+  },
+  {
+    "category": "相逢/同順",
+    "name": "四同順",
+    "points": "20",
+    "description": "四組一樣數字的順子"
+  },
+  {
+    "category": "相逢/同順",
+    "name": "五同順",
+    "points": "40",
+    "description": "五組一樣數字的順子"
+  },
+  {
+    "category": "相逢/同順",
+    "name": "小三色三連刻",
+    "points": "10",
+    "description": "三組不同門、數字相連的刻子/槓子/其中一組為眼"
+  },
+  {
+    "category": "相逢/同順",
+    "name": "大三色三連刻",
+    "points": "15",
+    "description": "三組不同門、數字相連的刻子/槓子"
+  },
+  {
+    "category": "么牌",
+    "name": "老少碰",
+    "points": "3",
+    "description": "同門（111）和（999），刻子或槓子皆可"
+  },
+  {
+    "category": "么牌",
+    "name": "雙老少碰",
+    "points": "15",
+    "description": "兩門老少碰"
+  },
+  {
+    "category": "么牌",
+    "name": "老少上",
+    "points": "3",
+    "description": "同門（123）和（789）"
+  },
+  {
+    "category": "么牌",
+    "name": "雙老少上",
+    "points": "6",
+    "description": "兩門老少上"
+  },
+  {
+    "category": "么牌",
+    "name": "斷么",
+    "points": "5",
+    "description": "全副牌沒有 1、9 及番子牌"
+  },
+  {
+    "category": "么牌",
+    "name": "混帶么",
+    "points": "30",
+    "description": "所有牌無論是上或碰都帶有一隻1/9，並包含番子牌"
+  },
+  {
+    "category": "么牌",
+    "name": "全帶么",
+    "points": "60",
+    "description": "全副牌無論是上或碰都帶有一隻1/9，並且沒有番子牌"
+  },
+  {
+    "category": "么牌",
+    "name": "混么",
+    "points": "80",
+    "description": "有番子牌，其餘牌都是1/9"
+  },
+  {
+    "category": "么牌",
+    "name": "清么",
+    "points": "120",
+    "description": "沒有番子牌，所有牌都是1/9"
+  },
+  {
+    "category": "步高",
+    "name": "明單色步步高",
+    "points": "10",
+    "description": "三組同門順子，每組第一個數字為遞增的連續數（+1），其中有明牌（123+234+345筒）"
+  },
+  {
+    "category": "步高",
+    "name": "暗單色步步高",
+    "points": "20",
+    "description": "三組同門順子，每組第一個數字為遞增的連續數（+1），全部藏於手牌（123+234+345筒）"
+  },
+  {
+    "category": "步高",
+    "name": "明三色步步高",
+    "points": "5",
+    "description": "筒、索、萬各一組順子，每組第一個數字為遞增的連續數（+1），其中有明牌（123萬+234索+345筒）"
+  },
+  {
+    "category": "步高",
+    "name": "暗三色步步高",
+    "points": "10",
+    "description": "筒、索、萬各一組順子，每組第一個數字為遞增的連續數（+1），全部藏於手牌（123萬+234索+345筒）"
+  },
+  {
+    "category": "步高",
+    "name": "明清龍",
+    "points": "10",
+    "description": "同一門集齊（1,2,3,4,5,6,7,8,9）相連的順子，其中有明牌"
+  },
+  {
+    "category": "步高",
+    "name": "暗清龍",
+    "points": "20",
+    "description": "同一門集齊（1,2,3,4,5,6,7,8,9）相連的順子，全部藏於手牌"
+  },
+  {
+    "category": "步高",
+    "name": "明雜龍",
+    "points": "5",
+    "description": "集齊筒、索、萬三門各一組順子，組成（1,2,3,4,5,6,7,8,9），其中有明牌"
+  },
+  {
+    "category": "步高",
+    "name": "暗雜龍",
+    "points": "10",
+    "description": "集齊筒、索、萬三門各一組順子，組成（1,2,3,4,5,6,7,8,9），全部藏於手牌"
+  },
+  {
+    "category": "四歸",
+    "name": "四歸一",
+    "points": "5",
+    "description": "同一隻牌有齊四隻，組成一組順子及一組刻子，其中有明牌"
+  },
+  {
+    "category": "四歸",
+    "name": "暗四歸一",
+    "points": "10",
+    "description": "同一隻牌有齊四隻，組成一組順子及一組刻子，全部藏於手牌"
+  },
+  {
+    "category": "四歸",
+    "name": "四歸二",
+    "points": "10",
+    "description": "同一隻牌有齊四隻，組成兩組順子及一對眼，其中有明牌"
+  },
+  {
+    "category": "四歸",
+    "name": "暗四歸二",
+    "points": "20",
+    "description": "同一隻牌有齊四隻，組成兩組順子及一對眼，全部藏於手牌"
+  },
+  {
+    "category": "四歸",
+    "name": "四歸四",
+    "points": "20",
+    "description": "同一隻牌有齊四隻，組成四組順子，其中有明牌"
+  },
+  {
+    "category": "四歸",
+    "name": "暗四歸四",
+    "points": "40",
+    "description": "同一隻牌有齊四隻，組成四組順子，全部藏於手牌"
+  },
+  {
+    "category": "八歸/十二歸",
+    "name": "八歸二",
+    "points": "10",
+    "description": "三組合用盡兩款四隻相同的牌，其中有明牌"
+  },
+  {
+    "category": "八歸/十二歸",
+    "name": "暗八歸二",
+    "points": "20",
+    "description": "三組合用盡兩款四隻相同的牌，全部藏於手牌"
+  },
+  {
+    "category": "八歸/十二歸",
+    "name": "十二歸3",
+    "points": "20",
+    "description": "四組合用盡三款四隻相同的牌，其中有明牌"
+  },
+  {
+    "category": "八歸/十二歸",
+    "name": "暗十二歸3",
+    "points": "40",
+    "description": "四組合用盡三款四隻相同的牌，全部藏於手牌"
+  },
+  {
+    "category": "一色",
+    "name": "混一色",
+    "points": "30",
+    "description": "筒、索、萬中只有其中一門，配搭番子牌"
+  },
+  {
+    "category": "一色",
+    "name": "清一色",
+    "points": "80",
+    "description": "筒、索、萬中只有其中一門，並且沒有番子牌"
+  },
+  {
+    "category": "一色",
+    "name": "字一色",
+    "points": "120",
+    "description": "全副牌由番子牌組成"
+  },
+  {
+    "category": "門",
+    "name": "缺一門",
+    "points": "5",
+    "description": "全副牌沒有番子牌，並且筒、索、萬三門只有其中兩門"
+  },
+  {
+    "category": "門",
+    "name": "小五門齊",
+    "points": "5",
+    "description": "集齊筒、索、萬、風牌和三元牌，其中四門三隻，一門兩隻"
+  },
+  {
+    "category": "門",
+    "name": "大五門齊",
+    "points": "10",
+    "description": "集齊筒、索、萬、風牌和三元牌，五門各三隻"
+  },
+  {
+    "category": "門",
+    "name": "小七門齊",
+    "points": "15",
+    "description": "集齊筒、索、萬、風牌和三元牌，其中四門三隻，一門兩隻，另加兩系列的花牌，至少一隻"
+  },
+  {
+    "category": "門",
+    "name": "大七門齊",
+    "points": "20",
+    "description": "集齊筒、索、萬、風牌和三元牌，五門各三隻，另加兩系列的花牌，至少一隻"
+  },
+  {
+    "category": "門",
+    "name": "缺五",
+    "points": "10",
+    "description": "全副牌中沒有數字 5 的牌"
+  },
+  {
+    "category": "滿庭芳",
+    "name": "混滿庭芳",
+    "points": "20",
+    "description": "有番子牌，其餘所有組合均帶有1個相同數字"
+  },
+  {
+    "category": "滿庭芳",
+    "name": "雙混滿庭芳",
+    "points": "30",
+    "description": "有番子牌，其餘所有組合均帶有2個相同數字"
+  },
+  {
+    "category": "滿庭芳",
+    "name": "三混滿庭芳",
+    "points": "35",
+    "description": "有番子牌，其餘所有組合均帶有3個相同數字"
+  },
+  {
+    "category": "滿庭芳",
+    "name": "純滿庭芳",
+    "points": "50",
+    "description": "沒有番子牌，所有組合均帶有1個相同數字"
+  },
+  {
+    "category": "特別牌型",
+    "name": "十六不搭",
+    "points": "40",
+    "description": "東南西北中發白＋筒索萬每門各3隻相隔至少3的牌＋這16隻任意1隻牌做眼"
+  },
+  {
+    "category": "特別牌型",
+    "name": "不搭雜龍",
+    "points": "20",
+    "description": "東南西北中發白＋筒索萬每門各3隻組成（1,4,7）（2,5,8）（3,6,9）的牌＋這16隻任意1隻牌做眼"
+  },
+  {
+    "category": "特別牌型",
+    "name": "不搭相逢",
+    "points": "20",
+    "description": "東南西北中發白＋筒索萬每門各3隻相隔至少3的牌，而3門的數字相同＋這16隻任意1隻牌做眼"
+  },
+  {
+    "category": "特別牌型",
+    "name": "嚦咕嚦咕",
+    "points": "40",
+    "description": "8對子＋這8對中任何一隻；如果有4隻相同可當作2對，只限門前清"
+  },
+  {
+    "category": "特別牌型",
+    "name": "八飛嚦咕",
+    "points": "50",
+    "description": "嚦咕嚦咕中，手持8對子，以叫8飛的方式食糊"
+  },
+  {
+    "category": "特別牌型",
+    "name": "三連對(嚦咕)",
+    "points": "3",
+    "description": "嚦咕嚦咕中，有三組同門、數字相連的對子"
+  },
+  {
+    "category": "特別牌型",
+    "name": "四連對(嚦咕)",
+    "points": "8",
+    "description": "嚦咕嚦咕中，有四組同門、數字相連的對子"
+  },
+  {
+    "category": "特別牌型",
+    "name": "五連對(嚦咕)",
+    "points": "20",
+    "description": "嚦咕嚦咕中，有五組同門、數字相連的對子"
+  },
+  {
+    "category": "特別牌型",
+    "name": "六連對(嚦咕)",
+    "points": "40",
+    "description": "嚦咕嚦咕中，有六組同門、數字相連的對子"
+  },
+  {
+    "category": "特別牌型",
+    "name": "七連對(嚦咕)",
+    "points": "80",
+    "description": "嚦咕嚦咕中，有七組同門、數字相連的對子"
+  },
+  {
+    "category": "特別牌型",
+    "name": "通天雜龍",
+    "points": "80",
+    "description": "三門中任意集齊［（11,22,33,44,55,66,77）+（888）］或［（22,33,44,55,66,77,88）+（111） 或 （999）］"
+  },
+  {
+    "category": "特別牌型",
+    "name": "通天清龍",
+    "points": "240",
+    "description": "同一門［（11,22,33,44,55,66,77）+（888）］或［（22,33,44,55,66,77,88） + （111）或（999）］"
+  },
+  {
+    "category": "特別牌型",
+    "name": "十三么",
+    "points": "100",
+    "description": "東南西北中發白＋1筒9筒1索9索1萬9萬＋一組任意順子/刻子"
+  },
+  {
+    "category": "特別牌型",
+    "name": "十三么(叫十三飛)",
+    "points": "120",
+    "description": "東南西北中發白＋1筒9筒1索9索1萬9萬＋一組任意順子/刻子，以叫13飛的方式食糊"
+  },
+  {
+    "category": "大/小",
+    "name": "混全大",
+    "points": "20",
+    "description": "有番子牌，其餘牌大於五（6,7,8,9）"
+  },
+  {
+    "category": "大/小",
+    "name": "清全大",
+    "points": "40",
+    "description": "沒有番子牌，所有牌大於五（6,7,8,9）"
+  },
+  {
+    "category": "大/小",
+    "name": "混全小",
+    "points": "20",
+    "description": "有番子牌，其餘牌小於五（1,2,3,4）"
+  },
+  {
+    "category": "大/小",
+    "name": "清全小",
+    "points": "40",
+    "description": "沒有番子牌，所有牌小於五（1,2,3.4）"
+  },
+  {
+    "category": "雞糊",
+    "name": "大雞糊",
+    "points": "30",
+    "description": "食糊時只有花牌的1番（無花/爛花），其餘組合不構成任何番數"
+  },
+  {
+    "category": "雞糊",
+    "name": "大鴨糊",
+    "points": "15",
+    "description": "自摸大雞糊"
+  },
+  {
+    "category": "絕",
+    "name": "四絕",
+    "points": "5",
+    "description": "叫糊的牌已有3隻在牌海和枱上的明牌中出現，食出最後1隻牌"
+  },
+  {
+    "category": "絕",
+    "name": "八絕",
+    "points": "10",
+    "description": "叫兩飛的情況下，已有7隻在牌海和枱上的明牌中出現，食出最後1隻牌"
+  },
+  {
+    "category": "絕",
+    "name": "十二絕",
+    "points": "20",
+    "description": "叫三飛的情況下，已有11隻在牌海和枱上的明牌中出現，食出最後1隻牌"
+  },
+  {
+    "category": "其他",
+    "name": "一炮雙響",
+    "points": "5",
+    "description": "一人出銃，兩家同時食糊"
+  },
+  {
+    "category": "其他",
+    "name": "一炮三響",
+    "points": "10",
+    "description": "一人出銃，三家同時食糊"
+  },
+  {
+    "category": "其他",
+    "name": "雙響報仇",
+    "points": "5",
+    "description": "上一局出銃一炮雙響，本局自摸"
+  },
+  {
+    "category": "其他",
+    "name": "三響報仇",
+    "points": "10",
+    "description": "上一局出銃一炮三響，本局自摸"
+  },
+  {
+    "category": "其他",
+    "name": "詐糊",
+    "points": "30",
+    "description": "打開所有手牌後，發現其實無法食糊"
+  },
+  {
+    "category": "其他",
+    "name": "搶槓",
+    "points": "10",
+    "description": "搶其他人加槓的牌"
+  }
+];
